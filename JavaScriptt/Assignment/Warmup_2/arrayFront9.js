d671b86f33909d799aadf3e672b9d627cd1e4cc6
Janak Pariya <janakpariya@gmail.com>
2022-07-05 18:28:01 +0530

    warmup_2 arrayFront9 done

---
+++ JavaScriptt/Assignment/Warmup_2/arrayFront9.js
@@ -0,0 +1,13 @@
+function arrayFront9(nums){
+    var count = 0;
+  for(var i=0; i<4; i++) {
+    if((nums[i] == 9)) {
+      count ++;
+    }
+  }
+  if(count > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
