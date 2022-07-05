aea21b7a1b92223a3e1f90031fce2d1b0ec8fed3
Janak Pariya <janakpariya@gmail.com>
2022-07-05 21:19:25 +0530

    warmup_2 array667 done

---
+++ JavaScriptt/Assignment/Warmup_2/array667.js
@@ -0,0 +1,9 @@
+function array667(nums) {
+  var count = 0;
+  for (var i = 0; i < nums.length - 1; i++) {
+    if (nums[i] == 6) {
+      if (nums[i + 1] == 6 || nums[i + 1] == 7) count++;
+    }
+  }
+  return count;
+}
