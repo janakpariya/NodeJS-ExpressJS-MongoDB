22ad071df56cb681218c2b5a0d16850ef23ba469
Janak Pariya <janakpariya@gmail.com>
2022-07-05 19:11:48 +0530

    warmup_2 altPair done

---
+++ JavaScriptt/Assignment/Warmup_2/altPairs.js
@@ -0,0 +1,12 @@
+function altPairs(str){
+  let a = "";
+  for (let i=0; i<str.length; i += 4) {
+    let end = i + 2;
+    if (end > str.length) {
+      end = str.length;
+    }
+    a = a + str.substring(i, end);
+  }
+  return result;
+
+}
