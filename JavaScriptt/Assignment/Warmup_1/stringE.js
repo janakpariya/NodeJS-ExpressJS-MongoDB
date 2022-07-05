d365fc15909dc42803e93b1967034a581be9b4b4
Janak Pariya <janakpariya@gmail.com>
2022-07-05 11:41:45 +0530

    warmup_1 stringE done

---
+++ JavaScriptt/Assignment/Warmup_1/stringE.js
@@ -0,0 +1,15 @@
+function stringE(str){
+    let count = 0;
+    for(var i=0; i< str.length; i++) {
+      if(str[i] == 'e') {
+        count++;
+      }
+    }
+    
+    if(count == 1 || count==3) {
+       return true
+    } else {
+      return false
+    }
+}
+  
