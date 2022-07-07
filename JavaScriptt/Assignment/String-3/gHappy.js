8bd4211181cd29dd69d71099b0804a15dae7240e
Janak Pariya <janakpariya@gmail.com>
2022-07-07 22:42:24 +0530

    string-3 gHappy

---
+++ JavaScriptt/Assignment/String-3/gHappy.js
@@ -0,0 +1,11 @@
+function gHappy(str) {
+  var happy = true;
+  for (var i = 0; i < str.length; i++) {
+    if (str.charAt(i) == "g") {
+      if (i > 0 && str.charAt(i - 1) == "g") happy = true;
+      else if (i < str.length - 1 && str.charAt(i + 1) == "g") happy = true;
+      else happy = false;
+    }
+  }
+  return happy;
+}
