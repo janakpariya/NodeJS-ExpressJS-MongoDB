9c91b65026cbcafc477c0813cd7b35934abdec22
Janak Pariya <janakpariya@gmail.com>
2022-07-08 14:41:15 +0530

    array-1 plusTwo

---
+++ JavaScriptt/Assignment/Array-1/plusTwo.js
@@ -0,0 +1,9 @@
+function plusTwo(a, b) {
+  const myArray = new Array();
+  myArray[0] = a[0];
+  myArray[1] = a[1];
+  myArray[2] = b[0];
+  myArray[3] = b[1];
+  return myArray;
+  //   return [a,b]
+}
