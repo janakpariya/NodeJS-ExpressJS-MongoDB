b6113f4885f6d4d30923f3be76f47421a46cd9c6
Janak Pariya <janakpariya@gmail.com>
2022-07-09 20:08:15 +0530

    recursion-1 countPairs

---
+++ JavaScriptt/Assignment/Recursion-1/countPairs.js
@@ -0,0 +1,5 @@
+function countPairs(str){
+  if (str=="" || str.length< 3) return 0;
+  if (str.charAt(0) == str.charAt(2)) return 1 + countPairs(str.substring(1));
+  else return countPairs(str.substring(1));
+}
